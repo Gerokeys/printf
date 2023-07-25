b46a23e66ce9dd401eb7b3c81c745bd8e482f388
j-blay <133429841+j-blay@users.noreply.github.com>
2023-07-25 18:54:20 +0000

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - function that evaluates if a char is printable
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
+
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
