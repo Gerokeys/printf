0cb71025d58ebac57bf8cf142c677fac4cfb1c25
j-blay <133429841+j-blay@users.noreply.github.com>
2023-07-25 18:49:39 +0000

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - function that calculates the size to cast the arg
+ * @format: Formatted string in which to print the args
+ * @i: List of args to be printed
  *
  * Return: Precision.
  */
+
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
